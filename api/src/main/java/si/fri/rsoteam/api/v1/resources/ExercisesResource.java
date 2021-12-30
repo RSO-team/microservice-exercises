1fed4c4390d1630ec2ece22a3f19df9792cf55ce
tstrus <tstrusplume.com>
2021-12-30 23:25:55 +0100

    service discovery to basketball-exercises

--- api/src/main/java/si/fri/rsoteam/api/v1/resources/ExercisesResource.java
+++ api/src/main/java/si/fri/rsoteam/api/v1/resources/ExercisesResource.java
@@ -1,10 +1,12 @@
 package si.fri.rsoteam.api.v1.resources;
 
+import com.kumuluz.ee.discovery.annotations.DiscoverService;
 import si.fri.rsoteam.lib.dtos.ExerciseDto;
 import si.fri.rsoteam.services.beans.ExercisesBean;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.inject.Provider;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -13,10 +15,13 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import java.net.URL;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 @ApplicationScoped
@@ -28,6 +33,10 @@ public class ExercisesResource {
     private Logger log = Logger.getLogger(ExercisesResource.class.getName());
 
     @Inject
+    @DiscoverService(value = "basketball-videos", environment = "dev", version = "1.0.0")
+    private Provider<Optional<WebTarget>> url;
+
+    @Inject
     private ExercisesBean exercisesBean;
 
     @Context
@@ -37,6 +46,19 @@ public class ExercisesResource {
     public Response getExercises() {
         return Response.ok(exercisesBean.getAllExercises()).build();
     }
+
+    @GET
+    @Path("/basketball")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getBasket() {
+        Optional<WebTarget> optionalUrl=url.get();
+        if(optionalUrl.isPresent()){
+            return Response.ok(optionalUrl.get().getUri().toString()).build();
+        }else{
+            return Response.noContent().build();
+        }
+    }
+
 
     @GET
     @Path("/{objectId}")
